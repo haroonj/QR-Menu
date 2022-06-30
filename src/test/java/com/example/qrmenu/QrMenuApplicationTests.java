1215181f95be8ca641bc5ea56846aacb12a191fd
haroonj <jj.haroon99@gmail.com>
2022-06-30 21:29:53 +0300

    first

---
+++ src/test/java/com/example/qrmenu/QrMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qrmenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrMenuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
